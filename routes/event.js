b95947778b99046682f5efc55ef56233290e9bfd
Seisan <mleffert@lhup.edu>
2016-04-19 22:20:40 -0400

    edited event route

--- routes/event.js
+++ routes/event.js
@@ -11,6 +11,45 @@ var eventCollection = bookshelf.Collection.extend({
 });
 
 eventController
+    .route('/:id')
+    .get(check, function(req, res, next){
+        Event.query({where: {eventId: req.params.id}})
+            .fetch()
+            .then(function(event){
+                res.send(event.toJSON());
+            });
+
+    })
+    .put(check, function( req, res, next){
+        Event.forge({eventId: req.params.id})
+            .fetch({require: true})
+            .then(function(event){
+                event.save({
+                     eventName: req.body.name           || event.get('eventName'),
+                     host: req.body.host                || event.get('host'),
+                     location: req.body.location        || event.get('location'),
+                     time: req.body.time                || event.get('time'),
+                     when: req.body.when                || event.get('when'),
+                     description: req.body.description  || event.get('description'),
+                                
+                }).then(function(event){
+                    res.send(event.toJSON());
+                })
+            });
+    })
+    .delete(check, function(req, res, next){
+        new Event({eventId: req.params.id})
+            .destroy()
+            .then(function(event){
+                res.status(200).json({message: 'success'});
+
+            }).catch(function (err){
+            res.status(500).json({message: err.message});
+        });
+
+    });
+
+eventController
     .route('/')
 
     .get(function(req, res, next){
